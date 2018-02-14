d5c2e654bb6e9c392457daa0fd12a24ee8ca5a27
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-02-14 18:37:26 +0900

    test

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 import os
 import subprocess
+command = "dpkg -l|grep ssh-server"
 app = Flask(__name__)
 MESSAGE = os.getenv('MESSAGE', 'Cannot load the env')
 SOME_API_KEY = os.getenv('SOME_API_KEY', None)
-SSH_PACKAGE = subprocess.check_output(['dpkg','-l','|','grep','ssh-server'])
+SSH_PACKAGE = subprocess.check_output([command])
 @app.route("/")
 def hello():
     #return MESSAGE
