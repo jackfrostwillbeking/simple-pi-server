74f52eff2158159489706a38b6e222975b770e23
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-02-14 12:16:43 +0900

    test

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ SOME_API_KEY = os.getenv('SOME_API_KEY', None)
 
 @app.route("/")
 def hello():
-    test = subprocess.call('dpkg -l|grep openssh-server', shell=True)
-    return test
+    MESSAGE = subprocess.call('dpkg -l|grep openssh-server', shell=True)
+    return MESSAGE
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
