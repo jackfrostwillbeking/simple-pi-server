928d7cecb43ec2685e73ef3bcca531cdec1997f2
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-02-14 18:32:29 +0900

    ssh check

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import subprocess
 app = Flask(__name__)
 MESSAGE = os.getenv('MESSAGE', 'Cannot load the env')
 SOME_API_KEY = os.getenv('SOME_API_KEY', None)
-
+SSH_PACKAGE = subprocess.check_output(['dpkg','-l'])
 @app.route("/")
 def hello():
-    return MESSAGE
-
+    #return MESSAGE
+    return SSH_PACKAGE
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
