f41b53bbbad850b4c4ac9c439b34845f869f4fbd
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-02-14 12:18:38 +0900

    repair

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ SOME_API_KEY = os.getenv('SOME_API_KEY', None)
 
 @app.route("/")
 def hello():
-    MESSAGE = subprocess.call('dpkg -l|grep openssh-server', shell=True)
     return MESSAGE
 
 if __name__ == "__main__":
